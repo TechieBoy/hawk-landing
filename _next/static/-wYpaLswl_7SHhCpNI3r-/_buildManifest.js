2fd5dc915f222dde050d506e78a2c19304ee01e8
Sanket Shanbhag <techieboy.teknas@gmail.com>
2021-10-14 20:05:26 +0530

    Fixed formatting

---
+++ _next/static/-wYpaLswl_7SHhCpNI3r-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0db4d7bf84ec58e74e53.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/roadmap":["static/chunks/pages/roadmap-bd9f9e17053b5f5360c0.js"],sortedPages:["/","/_app","/_error","/roadmap"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
